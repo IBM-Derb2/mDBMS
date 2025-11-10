02883c3247c331e37dd574312b5719e63a42cefa
Abdullah Farhan <19623305@std.stei.itb.ac.id>
2025-11-10 17:46:02 +0700

    feat: init log

---
+++ Failure_Recovery/log_config.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from dataclasses import dataclass, field
+from typing import Any, Union
+
+# tipe aksi yang bisa terjadi
+class ActionType(Enum):
+    START = 0
+    WRITE = 1
+    COMMIT = 2
+    ABORT = 3
+# mock  dari data yang dikirim oelh query processor
+@dataclass
+class MockExecutionResult:
+    transaction_id: int
+    query: str
+    # data bisa berisi apa aj, tapi utk write kita asumsikan datanya baru
+    data: Any 
+
+# moxk dari laporan perubahan yang dikasi oleh orang 2 (buffer manager) stlh dia memproces data
+@dataclass
+class MockChangeReport:
+    table_name: str
+    old_data: Union[dict, None] = None # data lama (utk update/delelte)
+    new_data: Union[dict, None] = None # data baru (utk update/insert)
+
