a8f74485939e9043765edbe3a8e70d2f9e07e13b
Aceng Ngaceng <13523082@std.stei.itb.ac.id>
2025-11-12 19:58:43 +0700

    feat: get_cost() with placeholder value

--- Query_Optimizer/optimization_engine.py
+++ Query_Optimizer/optimization_engine.py
@@ -3,7 +3,7 @@
 import logging
 from typing import Optional, Union
 
-from .types import ParsedQuery
+from .types import ParsedQuery, QueryTree
 from .lib.parse_query import internal_parse_query
 
 
@@ -69,6 +69,54 @@ class OptimizationEngine:
         Menghitung biaya eksekusi dari query yang diberikan,
         dan adalah method pendukung untuk method optimize_query.
         """
-        # TODO: Implement sophisticated cost calculation
-        # For now, return a placeholder cost
-        return 0
+        if isinstance(query, str):
+            parsed_query = self.parse_query(query)
+        else: parsed_query = query
+        return self._calculate_node_cost(parsed_query.query_tree)
+    
+    def _calculate_node_cost(self, node: QueryTree) -> int:
+        """
+        Menghitung biaya eksekusi dari sebuah node dalam pohon query secara rekursif.
+        Implementasi spesifik dari perhitungan biaya tergantung pada jenis node
+        dan atribut-atributnya.
+        """
+        node_cost = self._get_operation_cost(node)
+        children_cost = sum(self._calculate_node_cost(child) for child in node.children)
+        return node_cost + children_cost
+    
+    def _get_operation_cost(self, node: QueryTree) -> int:
+        """
+        Mendapatkan biaya operasi untuk jenis node tertentu.
+        Implementasi spesifik dari biaya tergantung pada jenis node.
+        """
+        if node.type in ['COLUMNS', 'FROM', 'WHERE', 'SET', 'VALUES', 'COLUMN_DEFS']:
+            return 0
+        elif node.type == 'TABLE':
+            return 50 #Placeholder Value
+        elif node.type == 'COLUMN':
+            return 1
+        elif node.type == 'LITERAL':
+            return 0
+        elif node.type == 'OPERATOR':
+            if node.val in ['AND', 'OR']:
+                return 50 #Placeholder Value
+            elif node.val in ['=', '!=', '<', '<=', '>', '>=', '<>']:
+                return 50 #Placeholder Value
+            elif node.val in ['+', '-', '*', '/', '%']:
+                return 50
+            else: #IN, BETWEEN, LIKE, ETC
+                return 50 #Placeholder Value
+        elif node.type == 'JOIN':
+            if node.val == 'NATURAL':
+                return 500 #Placeholder Value
+            else:
+                return 300 #Placeholder Value
+        elif node.type == 'SELECT':
+            return 50
+        elif node.type == 'INSERT':
+            return 100
+        elif node.type == 'UPDATE':
+            return 150
+        elif node.type == 'DELETE':
+            return 150
+        else: return 1
