6a83e299f292131f3ac935573b78e5ed330781d3
anellautari <19623229@std.stei.itb.ac.id>
2025-11-13 14:33:03 +0700

    feat: edit dataretrieval class

--- Storage_Manager/Utils.py
+++ Storage_Manager/Utils.py
@@ -1,37 +1,15 @@
-from __future__ import annotations
 from dataclasses import dataclass
-from typing import List, Optional, Literal
+from typing import List, Optional, Dict, Literal
 
-SearchType = Literal["linear", "index"]
-
+SearchMode = Literal["linear", "index"]
 
 @dataclass
 class DataRetrieval:
-    """
-    Atribut:
-    table : str
-        Nama tabel yang ingin diambil datanya
-    columns : list[str] | None
-        List nama kolom yang ingin diambil
-        None berarti ambil semua kolom (SELECT *)
-    conditions : list[Condition] | None
-        Kondisi seleksi (WHERE)
-    search_type : "linear" | "index"
-        "linear"  -> full scan ke file / semua blok
-        "index"   -> B+ tree / hash
-    index_column : str | None
-        Kolom yang digunakan sebagai key index ketika search_type = "index"
-        Kalau None, bisa fallback ke kolom di conditions[0]
-    """
-
     table: str
-    columns: Optional[List[str]] = None
-    conditions: Optional[List["Condition"]] = None
-    search_type: SearchType = "linear"
-    index_column: Optional[str] = None
-
-    def wants_all_columns(self) -> bool:
-        return self.columns is None or len(self.columns) == 0
+    column: list[str]
+    conditions: Optional[list[Condition]] = None
+    search_type: SearchMode = "linear"
+    index_column: Optional[str] = None 
 
 
 @dataclass
@@ -42,9 +20,8 @@ class DataDeletion:
 
 @dataclass
 class Statistic:
-    def __init__(self, n_r: int, b_r: int, l_r: int, f_r: int, V_a_r: dict[str, int]):
-        self.n_r = n_r
-        self.b_r = b_r
-        self.l_r = l_r
-        self.f_r = f_r
-        self.V_a_r = V_a_r
+    n_r: int
+    b_r: int
+    l_r: int
+    f_r: int
+    V_a_r: Dict[str, int]
