fffb4f5cfc1891315ad960ddceacca40f304fc43
yonatan-nyo <yonatan.njoto@gmail.com>
2025-11-13 20:12:23 +0700

    fixed types class source

--- Query_Optimizer/classes.py
+++ Query_Optimizer/classes.py
@@ -1,21 +1,4 @@
-from dataclasses import dataclass, field
-from typing import List, Optional
 from .optimization_engine import OptimizationEngine
-
-
-@dataclass
-class QueryTree:
-    type: str = "root"
-    val: str = "SELECT"
-    childs: List['QueryTree'] = field(default_factory=list)
-    parent: Optional['QueryTree'] = None
-
-
-@dataclass
-class ParsedQuery:
-    query_str: str
-    query_tree: QueryTree = field(default_factory=QueryTree)
-    plan_details: str = ""
-
+from .types import QueryTree, ParsedQuery
 
 __all__ = ['OptimizationEngine', 'QueryTree', 'ParsedQuery']
