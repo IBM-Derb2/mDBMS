df134dcea19847599c6e70c623a09c60215209c6
yonatan-nyo <yonatan.njoto@gmail.com>
2025-12-06 16:01:10 +0700

    allow negative

--- Query_Optimizer/lib/helpers/tokenizer.py
+++ Query_Optimizer/lib/helpers/tokenizer.py
@@ -143,8 +143,8 @@ class SQLTokenizer:
         if char in ('"', "'"):
             return self._read_string(char)
 
-        # Numbers
-        if char.isdigit():
+        # Numbers (including negative numbers)
+        if char.isdigit() or (char == '-' and self._peek(1) and self._peek(1).isdigit()):
             return self._read_number()
 
         # Identifiers and keywords
@@ -229,10 +229,15 @@ class SQLTokenizer:
         return SQLToken('STRING', value, start_pos)
 
     def _read_number(self) -> SQLToken:
-        """Read a numeric literal"""
+        """Read a numeric literal (including negative numbers)"""
         start_pos = self.position
         value = ''
         has_dot = False
+
+        # Handle negative sign
+        if self._peek() == '-':
+            value += '-'
+            self._advance()
 
         while self.position < len(self.query):
             char = self._peek()
